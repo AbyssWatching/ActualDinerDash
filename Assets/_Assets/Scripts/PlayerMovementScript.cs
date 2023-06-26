53fdab1044ce52a4baf55f33f8ef948fe860899c
Winston Grant <winstonf.grant@hotmail.com>
2023-06-26 16:52:24 -0700

    advanced mov
    when you walk up against a wall it now lets you slide up or down if pressing diagnols don't mind the logs :)

--- Assets/_Assets/Scripts/PlayerMovementScript.cs
+++ Assets/_Assets/Scripts/PlayerMovementScript.cs
@@ -22,25 +22,59 @@ public class PlayerMovementScript : MonoBehaviour
         PlayerMovement();
     }
 
-
     public void PlayerMovement()
     {
         // horizontalInput = Input.GetAxis("Horizontal");
-
         // verticalInput = Input.GetAxis("Vertical");
 
         Vector2 playerInput = PlayerInputScript.GetMovement();
 
         Vector3 direction = new Vector3(playerInput.x,0,playerInput.y);
+        float playerRadius = .7f;
+        float playerhieght = 2.2f;
+        float moveDistance = Time.deltaTime  * _speed ;
+        bool canMmove = !Physics.CapsuleCast(transform.position ,transform.position + Vector3.up * playerhieght, playerRadius, direction, moveDistance);
+        
+       
+        if(!canMmove)
+        {   
+            Vector3 directionX = new Vector3(direction.x,0,0);
 
-        transform.Translate(direction * _speed * Time.deltaTime, Space.World);
+            canMmove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerhieght, playerRadius, directionX, moveDistance);
+
+            if(canMmove)
+            {
+                direction = directionX;
+                Debug.Log("direction.x is working");
+            }
+
+            else
+            {
+                
+                Vector3 directionZ = new Vector3(0,0,direction.z);
+
+                canMmove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerhieght, playerRadius, directionZ, moveDistance);
+
+                if (canMmove)
+                {
+                    direction = directionZ;
+                    Debug.Log("having trouble with the y directional");
+                }
+            }
+        }
+
+        if(canMmove)
+        {
+            transform.Translate(direction * _speed * Time.deltaTime, Space.World);
+        }
 
         isWalking = direction != Vector3.zero;
 
         if(isWalking){
-        Quaternion rotation = Quaternion.LookRotation( direction, Vector3.up);
 
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, _rotationSpeed * Time.deltaTime);
+            Quaternion rotation = Quaternion.LookRotation( direction, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, _rotationSpeed * Time.deltaTime);
         }
     }
 
